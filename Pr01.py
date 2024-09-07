ec0bf2c884220c7bffb0fc7f8f910c6fba9771f3
AutoMaging <svyruchayeva@bk.ru>
2024-09-07 20:52:47 +0300

    update to good

--- Pr01.py
+++ Pr01.py
@@ -1,2 +1,19 @@
-km3okpl32wegfjkow4ejkp4gjop[]
-2whg
+class ArrayExample:
+    def main(self):
+        letters = ['f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h']
+        name = "Hello, "
+        a = [0] * 10
+
+        for i in range(len(letters)):
+            name += letters[i]
+            print(f"{name}! Count to {i + 1}")  # Выводим строку и текущий счёт
+
+        self.send_message(name, len(letters))
+
+    def send_message(self, name, msg):
+        print(f"{name}! Count to {msg}")
+
+
+if __name__ == "__main__":
+    example = ArrayExample()
+    example.main()
